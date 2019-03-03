1d450bb5d84fbc50216bc0202d49ae4307dbd192
U~man <em.ruaud@gmail.com>
2019-03-03 19:44:37 +0100

    Graphics: pushed desktop manager script

---
+++ graphics/scripts/data_library_com.lua
@@ -0,0 +1,10 @@
+function onInit()
+		DesktopManager.showDockTitleText(true);
+		DesktopManager.setDockTitleFont("sidebar");
+		DesktopManager.setDockTitleFrame("", 25, 2, 25, 5);
+		DesktopManager.setDockTitlePosition("bottom", 0, 1);
+		DesktopManager.setStackIconSizeAndSpacing(35, 22, 0, 0, 0, 0);
+		DesktopManager.setDockIconSizeAndSpacing(80, 80, 10, 10);
+		DesktopManager.setLowerDockOffset(0, 0, 0, 12);
+end
+
