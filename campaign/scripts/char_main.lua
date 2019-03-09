f3e2a5423514f86d92b0a2e9d4efa9558caf8e8b
Althea21 <althea0308@gmail.com>
2019-03-09 10:29:27 +0100

    refonte stats

--- campaign/scripts/char_main.lua
+++ campaign/scripts/char_main.lua
@@ -4,15 +4,20 @@
 --
 
 function onIntelligenceChanged()
+	intelligence.setValue(intelligence_base.getValue()+intelligence_modifier.getValue());
 end
 
 function onReflexChanged()
+	reflex.setValue(reflex_base.getValue()+reflex_modifier.getValue());
 end
 
 function onDexterityChanged()
+	dexterity.setValue(dexterity_base.getValue()+dexterity_modifier.getValue());
 end
 
 function onBodyChanged()
+	body.setValue(body_base.getValue()+body_modifier.getValue());
+	
 	local bodyValue = body.getValue();
 	encumbrancemax.setValue(bodyValue*10);
 	
@@ -40,17 +45,23 @@ function onBodyChanged()
 end
 
 function onSpeedChanged()
+	speed.setValue(speed_base.getValue()+speed_modifier.getValue());
+
 	run.setValue(speed.getValue()*3);
 	leap.setValue(math.floor(run.getValue()/5));
 end
 
 function onEmpathyChanged()
+	empathy.setValue(empathy_base.getValue()+empathy_modifier.getValue());
 end
 
 function onCraftingChanged()
+	crafting.setValue(crafting_base.getValue()+crafting_modifier.getValue());
 end
 
 function onWillChanged()
+	will.setValue(will_base.getValue()+will_modifier.getValue());
+	
 	local physical = math.floor((body.getValue() + will.getValue())/2);
 	hit_pointsmax.setValue(physical*5);
 	staminamax.setValue(physical*5);
