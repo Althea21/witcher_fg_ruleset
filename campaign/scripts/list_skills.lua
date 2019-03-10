f8ea5ffbeebd7db460320635d31803990400993b
Heedio <48034379+Heedio@users.noreply.github.com>
2019-03-10 16:23:37 +0100

    SKILLS list

---
+++ campaign/scripts/list_skills.lua
@@ -0,0 +1,56 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+local sFocus = "name";
+
+function addSkillsRoot()
+	local listattribut = {};
+	
+	for i, j in pairs(getWindows()) do
+		local label = j.name.getValue(); 
+		if SkillsTable.tbl_skills_root[label] then
+			if not listattribut[label] then
+				listattribut[label] = { j };
+			else
+				table.insert(listattribut[label], j);
+			end
+		end
+	end
+	for k, t in pairs(SkillsTable.tbl_skills_root) do
+		local matches = listattribut[k];
+		if not matches then
+			local newwin = createWindow();
+			newwin.name.setValue(k);
+			newwin.type.setValue(0);
+			matches = { newwin };
+			addSkills(k);
+		end
+	end
+end
+
+function addSkills(sCategorie)
+	local listskill = {};
+	
+	for i, j in pairs(getWindows()) do
+		local label = j.name.getValue(); 
+		if SkillsTable.tbl_skills_root[sCategorie][label] then
+			if not listskill[label] then
+				listskill[label] = { j };
+			else
+				table.insert(listskill[label], j);
+			end
+		end
+	end
+	for k, t in pairs(SkillsTable.tbl_skills_root[sCategorie]) do
+		local matches = listskill[k];
+		if not matches then
+			local newwin = createWindow();
+			newwin.name.setValue(t);
+			newwin.type.setValue(1);
+			matches = { newwin };
+		end
+	end
+
+end
