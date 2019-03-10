7d305d2b771f6abe8ab6a4629160a63ae1cda45f
Althea21 <althea0308@gmail.com>
2019-03-10 14:23:30 +0100

    Onglet combat

---
+++ scripts/manager_action_init.lua
@@ -0,0 +1,76 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+--
+-- Management for initiative rolls
+--
+
+OOB_MSGTYPE_APPLYINIT = "applyinit";
+
+function onInit()
+	-- Combat tracker syncing
+	OOBManager.registerOOBMsgHandler(OOB_MSGTYPE_APPLYINIT, handleApplyInit);
+	
+	-- Register the result handler - called after the dice have stopped rolling
+	ActionsManager.registerResultHandler("init", onResolve);
+end
+
+function handleApplyInit(msgOOB)
+	local rSource = ActorManager.getActor(msgOOB.sSourceType, msgOOB.sSourceNode);
+	local nTotal = tonumber(msgOOB.nTotal) or 0;
+
+	DB.setValue(ActorManager.getCTNode(rSource), "initresult", "number", nTotal);
+end
+
+function notifyApplyInit(rSource, nTotal)
+	if not rSource then
+		return;
+	end
+	
+	local msgOOB = {};
+	msgOOB.type = OOB_MSGTYPE_APPLYINIT;
+	
+	msgOOB.nTotal = nTotal;
+
+	local sSourceType, sSourceNode = ActorManager.getTypeAndNodeName(rSource);
+	msgOOB.sSourceType = sSourceType;
+	msgOOB.sSourceNode = sSourceNode;
+
+	Comm.deliverOOBMessage(msgOOB, "");
+end
+
+function getRoll(rActor, bSecretRoll)
+	local rRoll = {};
+	rRoll.sType = "init";
+	rRoll.aDice = { "d10" };
+	rRoll.nMod = 0;
+	
+	rRoll.sDesc = "[INIT]";
+	
+	rRoll.bSecret = bSecretRoll;
+
+	-- Determine the modifier and ability to use for this roll
+	local sAbility = nil;
+	local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);
+	if nodeActor then
+		rRoll.nMod = DB.getValue(nodeActor, "attributs.reflex", 0);
+	end
+	
+	return rRoll;
+end
+
+function performRoll(draginfo, rActor, bSecretRoll)
+	local rRoll = getRoll(rActor, bSecretRoll);
+	
+	ActionsManager.performAction(draginfo, rActor, rRoll);
+end
+
+function onResolve(rSource, rTarget, rRoll)
+	local rMessage = ActionsManager.createActionMessage(rSource, rRoll);
+	Comm.deliverChatMessage(rMessage);
+	
+	local nTotal = ActionsManager.total(rRoll);
+	notifyApplyInit(rSource, nTotal);
+end
