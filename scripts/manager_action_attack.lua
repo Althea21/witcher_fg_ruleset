5e153ec9b71cf4851e72e330a011d5da89c57faf
Heedio <48034379+Heedio@users.noreply.github.com>
2019-03-31 09:47:20 +0200

    Skill dice

--- scripts/manager_action_attack.lua
+++ scripts/manager_action_attack.lua
@@ -7,36 +7,25 @@
 -- Management for attack rolls
 --
 
--- Queue for tracking pending Attack for defense, then damage resolution (FIFO)
--- Each item must be formatted like :
---	* sSourceCT : offender (obtained by ActorManager.getCreatureNodeName(rSource) )
---	* sTargetCT : defender (obtained by ActorManager.getCTNodeName(rTarget) )
---	* nAtkValue : value of attack roll (total with modifier, reroll etc...)
---	* nDefValue : value of defense roll (total with modifier, reroll etc...)
--- Item must be cleared after damage resolution
-aAttackQueue = {}
-
 function onInit()
 	-- Register attack actions.  We'll allow use of the modifier stack for those actions.
 	GameSystem.actions["attack"] = { bUseModStack = true };
 	
-	-- Register modifier handler
-	ActionsManager.registerModHandler("attack", onAttackModifier);
-	
 	-- Register the result handler - called after the dice have stopped rolling
 	ActionsManager.registerResultHandler("attack", onAttackRoll);
+	ActionsManager.registerResultHandler("fumblereroll", onFumbleReroll);
 end
 
 -- method called by performAction to initiate the roll object which will be given 
 -- to high level ActionsManager to actually perform roll
 -- params :
 --	* rActor		: actor info retrieved by using ActorManager.resolveActor
---	* rWeapon		: weapon node
+--	* rAttack		: weapon type (supported : "melee", "range")
 --	* sAttackType	: attack type (supported : "fast", "strong", "normal"). 
 --					  Unknown or missing value will be trated like a "normal" attack
 -- returns : 
 --	* rRoll	: roll object
-function getRoll(rActor, rWeapon, sAttackType)
+function getRoll(rActor, rAttack, sAttackType)
 	-- Initialize a blank rRoll record
 	local rRoll = {};
 	
@@ -54,20 +43,6 @@ function getRoll(rActor, rWeapon, sAttackType)
 	rRoll.sExplodeMode  = "none";	-- initial roll, will be "fumble" or "crit" on reroll
 	rRoll.nTotalExplodeValue = 0; 	-- cumulative value of exploding rolls
 	rRoll.sStoredDice = "";			-- store all dice for final display message
-	rRoll.sWeaponType = "" ; 		-- range, melee, unarmed (used for fumble resolution)
-	
-	-- Add parameter for damage location, may be modified by modifier (see OnAttackModifier) or by rolling location table
-	rRoll.sDamageLocation = "";
-	
-	-- Debug.chat(rWeapon);
-	
-	if (rWeapon.range == "R") then
-		rRoll.sWeaponType = "range";
-	elseif (rWeapon.range == "M") then
-		rRoll.sWeaponType = "melee";
-	else
-		rRoll.sWeaponType = "unarmed";
-	end
 	
 	-- Look up actor / weapon specific information
 	local sActorType, nodeActor = ActorManager.getTypeAndNode(rActor);
@@ -76,35 +51,33 @@ function getRoll(rActor, rWeapon, sAttackType)
 		local nRollMod = 0;
 		
 		if sAttackType == "fast" then
-			sRollDescription = "[Fast attack with "..rWeapon.label.."]";
+			sRollDescription = "[Fast attack with "..rAttack.label.."]";
 		elseif sAttackType == "strong" then
-			sRollDescription = "[Strong attack with "..rWeapon.label.."][Strong -3]"; -- changing this may affect onAttackModifier function below
+			sRollDescription = "[Strong attack with "..rAttack.label.."][Strong -3]";
 			nRollMod = nRollMod - 3;
 		else
-			sRollDescription = "[Attack with "..rWeapon.label.."]";
+			sRollDescription = "[Attack with "..rAttack.label.."]";
 		end
 		
 		-- stat modifier
-		--Debug.chat("stat modifier ("..("attributs."..rWeapon.stat).."): "..DB.getValue(nodeActor, "attributs."..rWeapon.stat, 0));
-		nRollMod = nRollMod + DB.getValue(nodeActor, "attributs."..rWeapon.stat, 0);
-		sRollDescription = sRollDescription.."["..rWeapon.stat.." +"..DB.getValue(nodeActor, "attributs."..rWeapon.stat, 0).."]"
+		--Debug.chat("stat modifier ("..("attributs."..rAttack.stat).."): "..DB.getValue(nodeActor, "attributs."..rAttack.stat, 0));
+		nRollMod = nRollMod + DB.getValue(nodeActor, "attributs."..rAttack.stat, 0);
+		sRollDescription = sRollDescription.."["..rAttack.stat.." +"..DB.getValue(nodeActor, "attributs."..rAttack.stat, 0).."]"
 		
 		-- skill modifier
 		for _,v in pairs(nodeActor.getChild("skills.skillslist").getChildren()) do
-			if (DB.getValue(v, "name", "") == rWeapon.skill) then
-				--Debug.chat("skill modifier ("..rWeapon.skill.."): "..DB.getValue(v, "skill_value", 0));
+			if (DB.getValue(v, "name", "") == rAttack.skill) then
+				--Debug.chat("skill modifier ("..rAttack.skill.."): "..DB.getValue(v, "skill_value", 0));
 				nRollMod = nRollMod + DB.getValue(v, "skill_value", 0);
-				sRollDescription = sRollDescription.."["..rWeapon.skill.." +"..DB.getValue(v, "skill_value", 0).."]"
+				sRollDescription = sRollDescription.."["..rAttack.skill.." +"..DB.getValue(v, "skill_value", 0).."]"
 				break;
 			end
 		end
 		
 		-- weapon accuracy modifier
-		if (rWeapon.range ~= "U") then
-			--Debug.chat("weapon accuracy modifier : "..rWeapon.weaponaccuracy);
-			nRollMod = nRollMod + rWeapon.weaponaccuracy;
-			sRollDescription = sRollDescription.."[WA +"..rWeapon.weaponaccuracy.."]"
-		end
+		--Debug.chat("weapon accuracy modifier : "..rAttack.weaponaccuracy);
+		nRollMod = nRollMod + rAttack.weaponaccuracy;
+		sRollDescription = sRollDescription.."[WA +"..rAttack.weaponaccuracy.."]"
 		
 		-- TODO : ARMOR ENCUMBRANCE VALUE TO SUB
 		
@@ -118,36 +91,14 @@ end
 -- method called to initiate attack roll
 -- params :
 --	* draginfo		: info given when rolling from onDragStart event (nil if other event trigger the roll)
---	* rWeapon		: weapon node (actor node if unarmed attack)
---	* sAttackType	: attack type (supported : "fast", "strong", "normal", "punchfast", "punchstrong", "punchnormal", "kickfast", "kickstrong", "kicknormal"). 
---					  Unknown or missing value will be treated like a "normal" attack
+--	* sAttackType	: attack type (supported : "fast", "strong", "normal"). 
+--					  Unknown or missing value will be trated like a "normal" attack
 function performRoll(draginfo, rWeapon, sAttackType)
 	-- retreive attack info and actor node 
-	local rActor; 
-	
-	if (string.find(sAttackType, "punch") or string.find(sAttackType, "kick")) then
-		-- unarmed attack
-		rActor = rWeapon;
-		rWeapon = {};
-		rWeapon.range = "U";
-		if (string.find(sAttackType, "punch"))then
-			rWeapon.label = Interface.getString("char_label_punchlabel");
-		elseif (string.find(sAttackType, "kick"))then
-			rWeapon.label = Interface.getString("char_label_kicklabel");
-		end
-		rWeapon.stat = "reflex";
-		rWeapon.skill= "Brawling";
-		
-		sAttackType = string.gsub(sAttackType, "punch", "");
-		sAttackType = string.gsub(sAttackType, "kick", "");
-		
-	else
-		-- weapon attack
-		rActor, rWeapon = CharManager.getWeaponAttackRollStructures(rWeapon);
-	end
+	local rActor, rAttack = CharManager.getWeaponAttackRollStructures(rWeapon);
 	
 	-- get roll
-	local rRoll = getRoll(rActor, rWeapon, sAttackType);
+	local rRoll = getRoll(rActor, rAttack, sAttackType);
 	
 	-- roll it !
 	ActionsManager.performAction(draginfo, rActor, rRoll);
@@ -255,8 +206,6 @@ function onAttackRoll(rSource, rTarget, rRoll)
 					if i==1 and tonumber(aNewDice.result)==1 then
 						bFumble = true;
 						aNewDice.type="r10";
-					elseif bFumble then
-						aNewDice.result = 0-tonumber(aNewDice.result)
 					end
 					table.insert(aNewDices, aNewDice);
 				end
@@ -267,49 +216,11 @@ function onAttackRoll(rSource, rTarget, rRoll)
 		-- Create the base message based of the source and the final rRoll record (includes dice results).
 		local rMessage = ActionsManager.createActionMessage(rSource, rRoll);
 		
-		-- update rMessage in case of fumble
-		if (bFumble) then
-			rMessage.text = rMessage.text .. "\n[FUMBLE (".. rRoll.nTotalExplodeValue ..") : ";
-			-- check nTotalExplodeValue and attack type to resolve fumble
-			if (rRoll.nTotalExplodeValue <=5) then
-				rMessage.text = rMessage.text .. Interface.getString("fumble_none");
-			end
-				
-			if (rRoll.sWeaponType == "range") then
-				if (rRoll.nTotalExplodeValue >= 6 and rRoll.nTotalExplodeValue <= 7) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_range_6to7");
-				elseif (rRoll.nTotalExplodeValue >= 8 and rRoll.nTotalExplodeValue <= 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_range_8to9");
-				elseif (rRoll.nTotalExplodeValue > 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_range_over9");
-				end
-			elseif (rRoll.sWeaponType == "melee") then
-				if (rRoll.nTotalExplodeValue == 6) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_melee_6");
-				elseif (rRoll.nTotalExplodeValue == 7) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_melee_7");
-				elseif (rRoll.nTotalExplodeValue == 8) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_melee_8");
-				elseif (rRoll.nTotalExplodeValue == 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_melee_9");
-				elseif (rRoll.nTotalExplodeValue > 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_range_over9");
-				end
-			elseif (rRoll.sWeaponType == "unarmed") then
-				if (rRoll.nTotalExplodeValue == 6) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_unarmed_6");
-				elseif (rRoll.nTotalExplodeValue == 7) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_unarmed_7");
-				elseif (rRoll.nTotalExplodeValue == 8) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_unarmed_8");
-				elseif (rRoll.nTotalExplodeValue == 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_unarmed_9");
-				elseif (rRoll.nTotalExplodeValue > 9) then
-					rMessage.text = rMessage.text .. Interface.getString("fumble_unarmed_over9");
-				end
-			end
-
-			rMessage.text = rMessage.text .. "]";
+		-- update rMessage.diemodifier in case of fumble
+		if (fumble) then
+			-- total will be calculated as dice sum + modifier it's ok in standard case and crit case
+			-- but fumble total is : modifier - nTotalExplodeValue
+			-- TODO
 		end
 		
 		-- Debug.chat(rMessage);
@@ -317,136 +228,4 @@ function onAttackRoll(rSource, rTarget, rRoll)
 		-- Display the message in chat.
 		Comm.deliverChatMessage(rMessage);
 	end
-end
-
--- Modifier handler : additional modifiers to apply to the roll
-function onAttackModifier(rSource, rTarget, rRoll)
-	local aAddDesc = {};
-	local nAddMod = 0;
-	
-	-- Check modifiers
-	local bFastDraw = ModifierStack.getModifierKey("ATT_FSTDRAW");
-	local bStrongAttack = ModifierStack.getModifierKey("ATT_STRONG");
-	local bTargetSilhouetted = ModifierStack.getModifierKey("ATT_SILTGT");
-	local bAmbush = ModifierStack.getModifierKey("ATT_AMB");
-	local bTargetPinned = ModifierStack.getModifierKey("ATT_TGTPINNED");
-	local bTargetActiveDodge = ModifierStack.getModifierKey("ATT_TGTACTDODGE");
-	local bMovingTarget = ModifierStack.getModifierKey("ATT_MOVTGT");
-	local bRicochetShot = ModifierStack.getModifierKey("ATT_RIC");
-	local bLightLevelModifier = "daylight";
-	if ModifierStack.getModifierKey("LGT_BRI") then
-		bLightLevelModifier = "bright";
-	elseif ModifierStack.getModifierKey("LGT_DIM") then
-		bLightLevelModifier = "dim";
-	elseif ModifierStack.getModifierKey("LGT_DRK") then
-		bLightLevelModifier = "darkness";
-	end
-	
-	-- Aiming modifier. If aiming, this info must be stored for later damage resolution.
-	local sAimingModifier = "";
-	if ModifierStack.getModifierKey("AIM_HEAD") then
-		rRoll.sDamageLocation = "AIM_HEAD";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimhead").. " -6]");
-		nAddMod = nAddMod - 6;
-	elseif ModifierStack.getModifierKey("AIM_TORSO") then
-		rRoll.sDamageLocation = "AIM_TORSO";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimtorso").. " -1]");
-		nAddMod = nAddMod - 1;
-	elseif ModifierStack.getModifierKey("AIM_TAIL") then
-		rRoll.sDamageLocation = "AIM_TAIL";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimtail").. " -2]");
-		nAddMod = nAddMod - 2;
-	elseif ModifierStack.getModifierKey("AIM_ARM") then
-		rRoll.sDamageLocation = "AIM_ARM";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimarm").. " -3]");
-		nAddMod = nAddMod - 3;
-	elseif ModifierStack.getModifierKey("AIM_LEG") then
-		rRoll.sDamageLocation = "AIM_LEG";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimleg").. " -2]");
-		nAddMod = nAddMod - 2;
-	elseif ModifierStack.getModifierKey("AIM_LIMB") then
-		rRoll.sDamageLocation = "AIM_LIMB";
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_aiming").." : "..Interface.getString("modifier_label_aimlimb").. " -3]");
-		nAddMod = nAddMod - 3;
-	end
-	
-	-- if needed check range modifiers
-	local sRangeModifier = "";
-	if (rRoll.sWeaponType == "range") then
-		if ModifierStack.getModifierKey("RNG_PB") then
-			sRangeModifier = "pointblank";
-		elseif ModifierStack.getModifierKey("RNG_MED") then
-			sRangeModifier = "medium";
-		elseif ModifierStack.getModifierKey("DMG_LNG") then
-			sRangeModifier = "long";
-		elseif ModifierStack.getModifierKey("DMG_EXT") then
-			sRangeModifier = "extreme";
-		end
-	end
-	
-	
-	if bFastDraw then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_fstdraw").." -3]");
-		nAddMod = nAddMod - 3;
-	end
-	if bStrongAttack then
-		if not string.match(rRoll.sDesc, "%[Strong attack") then
-			table.insert(aAddDesc, "["..Interface.getString("modifier_label_atkstrong").." -3]");
-			nAddMod = nAddMod - 3;
-		end
-	end
-	if bTargetSilhouetted then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_silhouettedtarget").." +2]");
-		nAddMod = nAddMod + 2;
-	end
-	if bAmbush then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_ambush").." +5]");
-		nAddMod = nAddMod + 5;
-	end
-	if bTargetPinned then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_targetpinned").." +4]");
-		nAddMod = nAddMod + 4;
-	end
-	if bTargetActiveDodge then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_targetactdodge").." -2]");
-		nAddMod = nAddMod - 2;
-	end
-	if bMovingTarget then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_movtarget").." -3]");
-		nAddMod = nAddMod - 3;
-	end
-	if bRicochetShot then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_ricshot").." -5]");
-		nAddMod = nAddMod - 5;
-	end
-	if bLightLevelModifier == "bright" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_light_bright").." -3]");
-		nAddMod = nAddMod - 3;
-	elseif bLightLevelModifier == "darkness" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_light_dark").." -2]");
-		nAddMod = nAddMod - 2;
-	end
-	if sRangeModifier == "pointblank" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_range_pointblank").." +5]");
-		nAddMod = nAddMod + 5;
-	elseif sRangeModifier == "medium" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_range_medium").." -2]");
-		nAddMod = nAddMod - 2;
-	elseif sRangeModifier == "long" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_range_long").." -4]");
-		nAddMod = nAddMod - 4;
-	elseif sRangeModifier == "extreme" then
-		table.insert(aAddDesc, "["..Interface.getString("modifier_label_range_extreme").." -6]");
-		nAddMod = nAddMod - 6;
-	end
-	
-	if rSource then
-		-- TODO : Get condition modifiers
-	end
-	
-	if #aAddDesc > 0 then
-		rRoll.sDesc = rRoll.sDesc .. " " .. table.concat(aAddDesc, " ");
-	end
-	
-	rRoll.nMod = rRoll.nMod + nAddMod;
 end
