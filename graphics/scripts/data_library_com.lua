f490edc332e39a9f01987591ecc329094169cb39
U~man <em.ruaud@gmail.com>
2019-05-12 17:21:23 +0200

    ENH: reworked stack icons

--- graphics/scripts/data_library_com.lua
+++ graphics/scripts/data_library_com.lua
@@ -3,7 +3,7 @@ function onInit()
 		DesktopManager.setDockTitleFont("sidebar");
 		DesktopManager.setDockTitleFrame("", 25, 2, 25, 5);
 		DesktopManager.setDockTitlePosition("bottom", 0, 1);
-		DesktopManager.setStackIconSizeAndSpacing(32, 23, 1, 1, 0, 0);
+		DesktopManager.setStackIconSizeAndSpacing(25, 25, 6, 6, 0, 0);
 		DesktopManager.setStackOffset(0, 32, 0, 0);
 		DesktopManager.setDockIconSizeAndSpacing(90, 80, 6, 6);
 		DesktopManager.setLowerDockOffset(0, 0, 0, 10);
